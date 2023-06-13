bb293c6711f1642ae62ff8bcb4477fa86e192f91
Niklas <wi22b066@technikum-wien.at>
2023-06-13 09:46:47 +0200

    first commit

---
+++ src/main/java/com/example/slmfinal/SlmFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.slmfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slmfinal/SlmFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
